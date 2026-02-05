94cf632465e97470a123a573330b9dbe27f97527
Deva Mithra J <itsmithra137@gmail.com>
2026-02-05 18:53:16 +0530

    Update main.py
    Remove dotenv usage for Render

--- main.py
+++ main.py
@@ -4,12 +4,11 @@ Main FastAPI Application Entry Point
 """
 
 
-from dotenv import load_dotenv
 import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
-load_dotenv(dotenv_path=BASE_DIR / ".env")
+
 
 API_KEY = os.getenv("API_KEY")
 
